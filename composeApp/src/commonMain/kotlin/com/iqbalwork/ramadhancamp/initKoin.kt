e601c6af31026942ed07c71b1cc957685e9fafcd
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2026-02-23 11:54:13 +0700

    feat(di): register pray, quran, qibla, bookmark Koin modules

--- composeApp/src/commonMain/kotlin/com/iqbalwork/ramadhancamp/initKoin.kt
+++ composeApp/src/commonMain/kotlin/com/iqbalwork/ramadhancamp/initKoin.kt
@@ -1,12 +1,16 @@
 package com.iqbalwork.ramadhancamp
 
+import com.iqbalwork.ramadhancamp.feature.bookmark.di.bookmarkModule
 import com.iqbalwork.ramadhancamp.feature.home.di.homeModule
+import com.iqbalwork.ramadhancamp.feature.pray.di.prayModule
+import com.iqbalwork.ramadhancamp.feature.qibla.di.qiblaModule
+import com.iqbalwork.ramadhancamp.feature.quran.di.quranModule
 import com.iqbalwork.ramadhancamp.shared.common.navigation.di.navigationModule
 import org.koin.core.context.startKoin
 import org.koin.core.error.KoinApplicationAlreadyStartedException
 
 fun initKoin() {
     startKoin {
-        modules(navigationModule, homeModule)
+        modules(navigationModule, homeModule, prayModule, quranModule, qiblaModule, bookmarkModule)
     }
 }
